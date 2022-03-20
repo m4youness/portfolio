e9068d12785ac3a5edfdc951072d92d82f7f697b
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-19 20:33:08 -0400

    .

--- pages/index.js
+++ pages/index.js
@@ -24,30 +24,6 @@ function Page() {
           </Heading>
           <p>Digital Craftsman ( 3D Artist / Developer / Animator )</p>
         </Box>
-        <Box flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
-          textAlign="center">
-          <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow='hidden'
-          >
-            <Image
-              src="/images/MyBird.jpg"
-              alt="Profile image"
-              borderRadius='full'
-              width="100vh"
-              height="100vh"
-
-            />
-          </Box>
-        </Box>
       </Box>
       <Section delay={0.1}>
         <Heading as="h3" variant="section-title">
