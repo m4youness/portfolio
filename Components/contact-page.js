01f8b36446e1495455a3cddd0e06f605588eaadb
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-20 21:59:45 -0400

    .

--- Components/contact-page.js
+++ Components/contact-page.js
@@ -1,10 +1,15 @@
-import { Flex } from '@chakra-ui/react'
+import { Button, Flex, Input } from '@chakra-ui/react'
 
 function Contact_Page() {
  return (
-  <Flex>
-
-
+  <Flex height="100vh" justify-content="center" align-items="center">
+   <FLex direction="column" background="gray.100" p={12} rounded={6}>
+    <Heading mb={6}>Contact Me</Heading>
+    <Input placeholder='Name' variant="filled" mb={6} type="text" />
+    <Input placeholder='Email' variant="filled" mb={6} type="email" />
+    <Input placeholder='Message' variant="filled" mb={6} type="text" />
+    <Button colorScheme="teal">Send</Button>
+   </FLex>
   </Flex>
  )
 }
