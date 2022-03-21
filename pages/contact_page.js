cf3f8a0ae1435498e7a84064e2d1a65a5e06e92e
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-20 22:44:51 -0400

    .

--- pages/contact_page.js
+++ pages/contact_page.js
@@ -1,15 +1,15 @@
-import { Button, Flex, Input, Heading, Container, useColorModeValue } from '@chakra-ui/react'
+import { Button, Flex, Input, Heading, Container, useColorModeValue, Textarea } from '@chakra-ui/react'
 
 function Contact_Page() {
  return (
   <Container>
    <Flex height="100vh" as="section-title">
-    <Flex direction="column" background={useColorModeValue('gray.100', '-dark')} p={12} rounded={6}>
+    <Flex direction="column" background={useColorModeValue('gray.200', '-dark')} p={12} rounded={6}>
      <Heading mb={6}>Contact Us</Heading>
-     <Input placeholder='Name' variant="filled" mb={6} type="text" />
-     <Input placeholder='Email' variant="filled" mb={6} type="email" />
-     <Input placeholder='Message' variant="filled" mb={6} type="text" />
-     <Button colorScheme="teal">Send</Button>
+     <Input required placeholder='Name' variant="filled" mb={6} type="text" />
+     <Input required placeholder='Email' variant="filled" mb={6} type="email" />
+     <Textarea placeholder='Message' variant="filled" mb={6} />
+     <Button colorScheme="teal" type='submit'>Send</Button>
     </Flex>
    </Flex>
   </Container>
