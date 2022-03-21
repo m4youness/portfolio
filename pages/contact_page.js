29bce9e600042cbf318cc03a0b54fbb47d3ecfe2
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-21 19:32:20 -0400

    .

--- pages/contact_page.js
+++ pages/contact_page.js
@@ -1,11 +1,29 @@
 import { Button, Flex, Input, Heading, Container, useColorModeValue, Textarea, Fo } from '@chakra-ui/react'
 import { useForm } from 'react-hook-form';
 
-function Contact_Page() {
- const { register, handleSubmit, errors, reset } = useForm();
+import Axios from 'axios';
 
- function onSubmitForm() {
-  console.log(values);
+function Contact_Page() {
+ const { handleSubmit, reset } = useForm();
+
+
+ async function onSubmitForm(values) {
+  let config = {
+   method: 'post',
+   url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
+   headers: {
+    'Content-Type': 'application/json',
+   },
+   data: values,
+  };
+
+  try {
+   const response = await Axios(config);
+   console.log(response);
+   if (response.status == 200) {
+    reset();
+   }
+  } catch (err) { }
  }
 
  return (
@@ -14,24 +32,9 @@ function Contact_Page() {
     <Flex direction="column" background={useColorModeValue('gray.200', '-dark')} p={12} rounded={6}>
      <form onSubmit={handleSubmit(onSubmitForm)}>
       <Heading mb={6}>Contact Us</Heading>
-      <Input ref={register({
-       required: {
-        value: true,
-        message: 'You must enter your Name'
-       }
-      })} placeholder='Name' variant="filled" mb={6} type="text" />
-      <Input ref={register({
-       required: {
-        value: true,
-        message: 'You must enter your Email'
-       }
-      })} placeholder='Email' variant="filled" mb={6} type="email" />
-      <Textarea ref={register({
-       required: {
-        value: true,
-        message: 'You must enter a message!'
-       }
-      })} placeholder='Message' variant="filled" mb={6} />
+      <Input placeholder='Name' variant="filled" mb={6} type="text" />
+      <Input placeholder='Email' variant="filled" mb={6} type="email" />
+      <Textarea placeholder='Message' variant="filled" mb={6} />
       <Button colorScheme="teal" type='submit'>Send</Button>
      </form>
     </Flex>
