9452d2b8abe55afb9d34ac95d8b4cb61c96c7257
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-21 18:00:09 -0400

    .

--- pages/contact_page.js
+++ pages/contact_page.js
@@ -14,7 +14,12 @@ function Contact_Page() {
     <Flex direction="column" background={useColorModeValue('gray.200', '-dark')} p={12} rounded={6}>
      <form onSubmit={handleSubmit(onSubmitForm)}>
       <Heading mb={6}>Contact Us</Heading>
-      <Input ref={register} required placeholder='Name' variant="filled" mb={6} type="text" />
+      <Input ref={register({
+       required: {
+        value: true,
+        message: 'You must enter your name!'
+       }
+      })} placeholder='Name' variant="filled" mb={6} type="text" />
       <Input ref={register} required placeholder='Email' variant="filled" mb={6} type="email" />
       <Textarea ref={register} placeholder='Message' variant="filled" mb={6} />
       <Button colorScheme="teal" type='submit'>Send</Button>
