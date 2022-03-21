ad0cf222b5243e3dcad999b1031e39db636ebe28
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-21 17:57:34 -0400

    .

--- pages/contact_page.js
+++ pages/contact_page.js
@@ -1,15 +1,24 @@
-import { Button, Flex, Input, Heading, Container, useColorModeValue, Textarea } from '@chakra-ui/react'
+import { Button, Flex, Input, Heading, Container, useColorModeValue, Textarea, Fo } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form';
 
 function Contact_Page() {
+ const { register, handleSubmit, errors, reset } = useForm();
+
+ function onSubmitForm() {
+  console.log(values);
+ }
+
  return (
   <Container>
    <Flex height="100vh" as="section-title">
     <Flex direction="column" background={useColorModeValue('gray.200', '-dark')} p={12} rounded={6}>
-     <Heading mb={6}>Contact Us</Heading>
-     <Input required placeholder='Name' variant="filled" mb={6} type="text" />
-     <Input required placeholder='Email' variant="filled" mb={6} type="email" />
-     <Textarea placeholder='Message' variant="filled" mb={6} />
-     <Button colorScheme="teal" type='submit'>Send</Button>
+     <form onSubmit={handleSubmit(onSubmitForm)}>
+      <Heading mb={6}>Contact Us</Heading>
+      <Input ref={register} required placeholder='Name' variant="filled" mb={6} type="text" />
+      <Input ref={register} required placeholder='Email' variant="filled" mb={6} type="email" />
+      <Textarea ref={register} placeholder='Message' variant="filled" mb={6} />
+      <Button colorScheme="teal" type='submit'>Send</Button>
+     </form>
     </Flex>
    </Flex>
   </Container>
