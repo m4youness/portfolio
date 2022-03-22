309b2d82eb7d503ef217d6d40fd9ba581b12153b
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-21 21:01:47 -0400

    contact form

--- pages/contact_page.js
+++ pages/contact_page.js
@@ -1,40 +1,35 @@
-import { Button, Flex, Input, Heading, Container, useColorModeValue, Textarea, Fo } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form';
+import { Button, Flex, Input, Heading, Container, useColorModeValue, Textarea } from '@chakra-ui/react'
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react'
 
-import Axios from 'axios';
 
 function Contact_Page() {
- const { handleSubmit, reset } = useForm();
+ const form = useRef();
+
+ const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs.sendForm('gmail', 'template_ouyawmw', form.current, 'NZRkaK2Sv2rlabx3F')
+   .then((result) => {
+    console.log(result.text);
+   }, (error) => {
+    console.log(error.text);
+   });
+  e.target.reset()
+ };
 
 
- async function onSubmitForm(values) {
-  let config = {
-   method: 'post',
-   url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
-   headers: {
-    'Content-Type': 'application/json',
-   },
-   data: values,
-  };
-
-  try {
-   const response = await Axios(config);
-   console.log(response);
-   if (response.status == 200) {
-    reset();
-   }
-  } catch (err) { }
- }
 
  return (
   <Container>
    <Flex height="100vh" as="section-title">
     <Flex direction="column" background={useColorModeValue('gray.200', '-dark')} p={12} rounded={6}>
-     <form onSubmit={handleSubmit(onSubmitForm)}>
+     <form ref={form} onSubmit={sendEmail}>
       <Heading mb={6}>Contact Us</Heading>
-      <Input placeholder='Name' variant="filled" mb={6} type="text" />
-      <Input placeholder='Email' variant="filled" mb={6} type="email" />
-      <Textarea placeholder='Message' variant="filled" mb={6} />
+      <Input name='name' placeholder='Name' variant="filled" mb={6} type="text" />
+      <Input name='email' placeholder='Email' variant="filled" mb={6} type="email" />
+      <Input name='subject' placeholder='Subject' variant="filled" mb={6} type="text" />
+      <Textarea name='message' placeholder='Message' variant="filled" mb={6} />
       <Button colorScheme="teal" type='submit'>Send</Button>
      </form>
     </Flex>
