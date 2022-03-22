06c3e5a4cfb99a7057eaae6959e01c698e18baf8
Mohamad Youness <pixlerfrost@gmail.com>
2022-03-21 21:31:35 -0400

    .

--- pages/contact_page.js
+++ pages/contact_page.js
@@ -1,15 +1,16 @@
 import { Button, Flex, Input, Heading, Container, useColorModeValue, Textarea } from '@chakra-ui/react'
 import emailjs from '@emailjs/browser';
 import { useRef } from 'react'
+import { init } from '@emailjs/browser';
+init("NZRkaK2Sv2rlabx3F");
 
-
-function Contact_Page() {
+export function Contact_Page() {
  const form = useRef();
 
  const sendEmail = (e) => {
   e.preventDefault();
 
-  emailjs.sendForm('gmail', 'template_ouyawmw', form.current, 'NZRkaK2Sv2rlabx3F')
+  emailjs.sendForm('service_k3o1pqk', 'template_ouyawmw', form.current, 'NZRkaK2Sv2rlabx3F')
    .then((result) => {
     console.log(result.text);
    }, (error) => {
@@ -26,11 +27,11 @@ function Contact_Page() {
     <Flex direction="column" background={useColorModeValue('gray.200', '-dark')} p={12} rounded={6}>
      <form ref={form} onSubmit={sendEmail}>
       <Heading mb={6}>Contact Us</Heading>
-      <Input name='name' placeholder='Name' variant="filled" mb={6} type="text" />
-      <Input name='email' placeholder='Email' variant="filled" mb={6} type="email" />
-      <Input name='subject' placeholder='Subject' variant="filled" mb={6} type="text" />
-      <Textarea name='message' placeholder='Message' variant="filled" mb={6} />
-      <Button colorScheme="teal" type='submit'>Send</Button>
+      <Input name='name' id='name' placeholder='Name' variant="filled" mb={6} type="text" />
+      <Input name='email' id='email' placeholder='Email' variant="filled" mb={6} type="email" />
+      <Input name='subject' id='subject' placeholder='Subject' variant="filled" mb={6} type="text" />
+      <Textarea name='message' id='message' placeholder='Message' variant="filled" mb={6} />
+      <Button colorScheme="teal" type='submit' value="send">Send</Button>
      </form>
     </Flex>
    </Flex>
