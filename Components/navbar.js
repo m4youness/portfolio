8b230a4fcdb199398df7a5f564608bfaea7be754
Mohamad Youness <Pixlerfrost@gmail.com>
2022-04-09 20:03:44 -0400

    Update navbar.js

--- Components/navbar.js
+++ Components/navbar.js
@@ -28,7 +28,7 @@ const Navbar = props => {
   const { path } = props
 
   return (
-    <Box position="fixed " as="nav" w="100%" bg={useColorModeValue('#ffffff40', '#20202380')} style={{ backdropFilter: 'blur(5px' }} zIndex={1} {...props}>
+    <Box position="fixed " as="nav" w="100%" bg={useColorModeValue('#ffffff40', '#20202380')} style={{ backdropFilter: 'blur(10px' }} zIndex={1} {...props}>
       <Container display="flex" p={2} maxW="container.md" wrap="wrap" align="center" justify="space-between" >
         <Flex align="center" mr={5} >
           <Heading as="h1" size="md" mb={1} letterSpacing={'tighter'}>
