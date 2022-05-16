818e3d449798c8705a17b6c6850c2071b1ba366a
Mohamad Youness <Pixlerfrost@gmail.com>
2022-05-15 20:59:23 -0400

    Update navbar.js

--- Components/navbar.js
+++ Components/navbar.js
@@ -31,7 +31,7 @@ const Navbar = props => {
     <Box position="fixed " as="nav" w="100%" bg={useColorModeValue('#ffffff40', 'whiteAlpha.50')} style={{ backdropFilter: 'blur(10px' }} zIndex={1} {...props}>
       <Container display="flex" p={2} maxW="container.md" wrap="wrap" align="center" justify="space-between" >
         <Flex align="center" mr={5} >
-          <Heading as="h1" size="md" mb={1} letterSpacing={'tighter'}>
+          <Heading as="h1" size="md" pt={0.5} letterSpacing={'tighter'}>
             <Logo />
           </Heading>
         </Flex>
