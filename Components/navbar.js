640afb807290fb59ba3129fb6aea60d20b8661c0
Mohamad Youness <mehdi@Mohamads-Laptop.local>
2024-11-21 17:01:10 +0200

    fixed ui for mobile

--- Components/navbar.js
+++ Components/navbar.js
@@ -1,14 +1,9 @@
-import NextLink from "next/link";
 import {
   Container,
   Box,
-  Link,
-  Stack,
   Heading,
   Flex,
   Menu,
-  MenuItem,
-  MenuList,
   MenuButton,
   IconButton,
   useColorModeValue,
@@ -16,10 +11,8 @@ import {
 import { HamburgerIcon } from "@chakra-ui/icons";
 import ThemeToggleButton from "../Components/theme-toggle-button";
 import Logo from "../Components/logo";
-import { IoLogoGithub } from "react-icons/io5";
 
 const Navbar = (props) => {
-  const linkColor = useColorModeValue("black", "white");
   const bgColor = useColorModeValue("#ffffff40", "whiteAlpha.50");
 
   return (
@@ -45,34 +38,9 @@ const Navbar = (props) => {
             <Logo />
           </Heading>
         </Flex>
-        {/* <Stack */}
-        {/*   direction={{ base: "column", md: "row" }} */}
-        {/*   display={{ base: "none", md: "flex" }} */}
-        {/*   width={{ base: "full", md: "auto" }} */}
-        {/*   alignItems="center" */}
-        {/*   flexGrow={1} */}
-        {/*   mt={{ base: 4, md: 0 }} */}
-        {/* > */}
-        {/*   <Stack */}
-        {/*     direction={{ md: "row" }} */}
-        {/*     display={{ md: "flex" }} */}
-        {/*     width={{ md: "45" }} */}
-        {/*     alignItems="center" */}
-        {/*   ></Stack> */}
-        {/* </Stack> */}
 
         <Box flex={1} textAlign="right">
           <ThemeToggleButton />
-          <Box ml={2} display={{ base: "inline-block", md: "none" }}>
-            <Menu isLazy id="navbar-menu">
-              <MenuButton
-                as={IconButton}
-                icon={<HamburgerIcon />}
-                variant="outline"
-                aria-label="Options"
-              />
-            </Menu>
-          </Box>
         </Box>
       </Container>
     </Box>
